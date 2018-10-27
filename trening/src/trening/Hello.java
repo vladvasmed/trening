3d5e7d65c7aab7c614b905fcfae6b2d269c04549
vladsuperman <vladsuperman@gmail.com>
2018-10-27 21:32:42 +0300

    weqw

---
+++ trening/src/trening/Hello.java
@@ -0,0 +1,13 @@
+package trening;
+
+public class Hello
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("XXXXXXXXXXXXXXXXXXXXX");
+		// TODO Auto-generated method stub
+
+	}
+
+}
