29ced0593570ebab563d85d52659022c25ddc499
vladsuperman <vladsuperman@gmail.com>
2018-10-27 20:35:02 +0300

    hello

---
+++ trening/src/trening/Runner.java
@@ -0,0 +1,14 @@
+package trening;
+
+public class Runner
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello!!! WORLD!!!");
+		
+
+	}
+
+}
